dddd506d84c6091edce93a53b20233b02f1d1474
Tatsu <tatsuhiroabe0127@gmail.com>
2020-09-13 11:47:16 +0900

    #4-createPostEdit

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,4 +6,6 @@
 {{ $post->url }}
 {{ $post->updated_at }}
 <img src="{{ $post->image_path }}" alt="画像">
+<a href="{{ route('posts.edit', $post->id) }}">編集</a>
+<a href="{{ route('posts.index') }}">戻る</a>
 @endsection
