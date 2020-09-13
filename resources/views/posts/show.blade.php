b28782394610a4452345fdc08763c2ce7da6585f
Tatsu <tatsuhiroabe0127@gmail.com>
2020-09-13 11:06:41 +0900

    #3-createPostShowPage

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,15 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<a href="{{ route('posts.create') }}">記事を投稿する</a>
-<br>
-@foreach($posts as $post)
 {{ $post->title }}
 {{ $post->content }}
 {{ $post->url }}
 {{ $post->updated_at }}
 <img src="{{ $post->image_path }}" alt="画像">
-<a href="{{ route('posts.show', $post->id) }}">詳細</a>
-<br>
-@endforeach
 @endsection
