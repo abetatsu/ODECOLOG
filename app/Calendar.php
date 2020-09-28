2f8f3334465b1b4f6f314457bf4d7d04b1cbb63c
Tatsu <tatsuhiroabe0127@gmail.com>
2020-09-28 20:30:53 +0900

    issue-52-ODECOLOG

--- app/Calendar.php
+++ app/Calendar.php
@@ -38,25 +38,27 @@ class Calendar
           $lastDay = Carbon::create($year, $month, 1)->daysInMonth;
           $day = 1 - $firstWeekDay;
           $this->html = <<< EOS
-          <h1 class="mx-auto">
-          <a href="/records/?year={$prev_year}&month={$prev_month}" role="button"><img src="https://res.cloudinary.com/tatsu/image/upload/v1601012680/chevron-left_yrrcyg.svg"></a>
+          <h1 class="calendar-top">
+          <a href="/records/?year={$prev_year}&month={$prev_month}" role="button"><img src="https://res.cloudinary.com/tatsu/image/upload/v1601012680/chevron-left_yrrcyg.svg" class="calendar-arrow"></a>
           {$year} / {$month}
-          <a href="/records/?year={$next_year}&month={$next_month}" role="button"><img src="https://res.cloudinary.com/tatsu/image/upload/v1601012718/chevron-right_dddbda.svg"></a>
+          <a href="/records/?year={$next_year}&month={$next_month}" role="button"><img src="https://res.cloudinary.com/tatsu/image/upload/v1601012718/chevron-right_dddbda.svg" class="calendar-arrow"></a>
           </h1>
           <table class="table table-bordered">
+          <thead>
           <tr>
-          <th scope="col">日</th>
+          <th scope="col" class="red">日</th>
           <th scope="col">月</th>
           <th scope="col">火</th>
           <th scope="col">水</th>
           <th scope="col">木</th>
           <th scope="col">金</th>
-          <th scope="col">土</th>
-          </th>
+          <th scope="col" class="blue">土</th>
+          </tr>
+          </thead>
 EOS;
 
           while ($day <= $lastDay) {
-               $this->html .= "<tr>";
+               $this->html .= "<tbody><tr>";
                for ($i = 0; $i < 7; $i++) {
                     if ($day <= 0 || $day > $lastDay) {
                          $this->html .= "<td>&nbsp;</td>";
@@ -66,7 +68,7 @@ EOS;
                          foreach ($this->records as $record) {
                               if ($record->day == $target && $record->user_id == Auth::id() ) {
                                    $this->html .= <<< EOS
-                                   <a href="/records/{$record->id}">●</a>
+                                   <a href="/records/{$record->id}"><img src="https://res.cloudinary.com/tatsu/image/upload/v1601289851/check_gtaxat.svg" alt="check" class="calendar-check"></a>
                                    EOS;
                                    break;
                               }
@@ -75,7 +77,7 @@ EOS;
                     }
                     $day++;
                }
-               $this->html .= "</tr>";
+               $this->html .= "</tr></tbody>";
           }
           return $this->html .= '</table>';
      }
