496303b54ac6033eda973dc2227219fcbafc5287
Tatsu <tatsuhiroabe0127@gmail.com>
2020-10-15 21:09:54 +0900

    changeLetterOnOGP

--- resources/views/layouts/ogp-meta.blade.php
+++ resources/views/layouts/ogp-meta.blade.php
@@ -3,4 +3,3 @@
 <meta property="og:url"         content="https://odecolog.herokuapp.com/">
 <meta name="twitter:card"       content="summary">
 <meta property="og:image"       content="https://res.cloudinary.com/tatsu/image/upload/v1602546933/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88_2020-10-13_8.55.04_fvpo4k.png">
-<meta property="og:site_name" content="ODECOLOG - おでこの生え際が気になる方へ" />
