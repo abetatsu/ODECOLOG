bcabc93ae14a46e0745fb65a9a89652576ba7661
Tatsu <tatsuhiroabe0127@gmail.com>
2020-10-19 22:50:10 +0900

    issue-157-ODECOLOG

--- resources/views/layouts/_head.blade.php
+++ resources/views/layouts/_head.blade.php
@@ -25,20 +25,20 @@
      @include('layouts.ogp-meta')
 
      <!-- Scripts -->
-     <script src="{{ secure_asset('js/app.js') }}" defer></script>
+     <script src="{{ asset('js/app.js') }}" defer></script>
 
      <!-- Fonts -->
      <link rel="dns-prefetch" href="//fonts.gstatic.com">
      <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
      <!-- Styles -->
-     <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-     <link rel="apple-touch-icon" sizes="180x180" href="{{ secure_asset('image/apple-touch-icon.png') }}">
-     <link rel="icon" type="image/png" sizes="32x32" href="{{ secure_asset('image/favicon-32x32.png') }}">
-     <link rel="icon" type="image/png" sizes="16x16" href="{{ secure_asset('image/favicon-16x16.png') }}">
-     <link rel="manifest" href="{{ secure_asset('image/site.webmanifest') }}">
-     <link rel="mask-icon" href="{{ secure_asset('image/safari-pinned-tab.svg') }}" color="#5bbad5">
+     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('image/apple-touch-icon.png') }}">
+     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('image/favicon-32x32.png') }}">
+     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('image/favicon-16x16.png') }}">
+     <link rel="manifest" href="{{ asset('image/site.webmanifest') }}">
+     <link rel="mask-icon" href="{{ asset('image/safari-pinned-tab.svg') }}" color="#5bbad5">
      <meta name="msapplication-TileColor" content="#da532c">
      <meta name="theme-color" content="#ffffff">
 </head>
